cab7581b97d4f2b5d27d72a635007983f3035a1e
TwinTurbo2JZ <71554683+TwinTurbo2JZ@users.noreply.github.com>
2021-09-21 17:32:55 +0530

    remove from cart completed

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -30,7 +30,10 @@ const rootReducer = (state = defaultState, action) => {
   }
 
   if (action.type === REMOVE) {
-    return { ...state };
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload.id),
+    };
   }
 
   return state;
