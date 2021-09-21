c5ae7910e0058e6942a430d33006f192dbb11e21
TwinTurbo2JZ <71554683+TwinTurbo2JZ@users.noreply.github.com>
2021-09-21 19:50:01 +0530

    adding decrease item logic in redux

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils";
 import { combineReducers } from "redux";
 import cartItems from "../cart-items";
 import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from "./cart/cartVariables";
@@ -11,13 +12,27 @@ const defaultState = {
 const rootReducer = (state = defaultState, action) => {
   // console.log({ state, action });
   if (action.type === DECREASE) {
-    return { ...state };
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
+    } else {
+      tempCart = state.cart.map((item) => {
+        if (action.payload.id === item.id) {
+          item = { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      });
+    }
+
+    return { ...state, cart: tempCart };
   }
 
   if (action.type === INCREASE) {
     let tempCart = state.cart.map((item) => {
       if (action.payload.id === item.id) {
-        // console.log(item);
         item = { ...item, amount: item.amount + 1 };
       }
       return item;
