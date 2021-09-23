2ca87e7877b72bccc0a704a83f669eae4f884f84
TwinTurbo2JZ <71554683+TwinTurbo2JZ@users.noreply.github.com>
2021-09-24 01:36:29 +0530

    adding useEffect dependencies

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -6,7 +6,7 @@ import { CLEAR_CART, GET_TOTALS } from "../state/cart/cartVariables";
 const CartContainer = ({ cart = [], total, dispatch }) => {
   useEffect(() => {
     dispatch({ type: GET_TOTALS });
-  });
+  }, [cart, dispatch]);
   if (cart.length === 0) {
     return (
       <section className="cart">
